5947e09332f835b6106e2f3258308291b72dab14
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-01-18 21:45:28 +0200

    Very rough draft of a cabinet inside the Auth page for now

--- src/pages/MonobankAuth.tsx
+++ src/pages/MonobankAuth.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { MONOBANK_API_URL, MONOBANK_TOKEN_KEY } from "../constants";
 import LocalStorage from "../utils/localStorage";
+import './PersonalAccount.css';
 
 function MonobankAuth() {
   const [personalData, setPersonalData] = useState<any>();
@@ -41,12 +42,35 @@ function MonobankAuth() {
       </p>
 
       {personalData && (
-        <div>
-          <h2>Hello, {personalData.name}</h2>
-        </div>
+        <main>
+          <h1>Your cabinet, {personalData.name}</h1>
+          <section className="section">
+            <p className="bankName">Name Bank (available accounts *numbers*)</p>
+            <button className="viewAllBtn">View all accounts </button>
+            <p>Total amount for this Bank - *numbers*</p>
+            <ul className="list">
+              {personalData.accounts.map((account: any) => {
+                return (
+                  <li key={account.id} className="listItem">
+                    <div className="cardBox">
+                      <p>{account.currencyCode === 980 ? 'UAH' : 'USD'}</p>
+                      <p>{account.balance / 100}</p>
+                      <p>{account.maskedPan}</p>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+            <button>Add new bank</button>
+          </section>
+        </main>
       )}
     </div>
   );
 }
 
 export default MonobankAuth;
+
+
+
+
